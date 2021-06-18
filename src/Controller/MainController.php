3f66035efd5cfae674d5f12154aed221fa0dab3b
Fran Grenko <fran.grenko@gmail.com>
2021-06-18 15:09:08 +0200

    Removed connection with ElasticSearch via curl and added elastic/elasticsearch-php client for easier indexing and querying. Added search results page with displaying links to pdf files (which open when you click on them)

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use Elasticsearch\ClientBuilder;
 use PhpParser\Node\Scalar\MagicConst\Dir;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     /**
-     * @Route("/login1", name="login")
+     * @Route("/login", name="login")
      */
     public function index(): Response
     {
@@ -25,18 +27,56 @@ class MainController extends AbstractController
 
     }
     /**
-     * @Route("/success", name="app_homepage")
+     * @Route ("/search", name="search")
+     * @param Request $request
      */
+    public function search(Request $request): Response{
+        $arr = array();
+        $hosts = [
+            'http://elasticsearch:9200',       // HTTP Basic Authentication
+        ];
+        $client = ClientBuilder::create()->setHosts($hosts)->build();
+        $content = $request->get('search');
 
-    public function afterLogin(){
-        require __DIR__ . '../Functions/start.php';
-        return new Response('Sve je dobro.');
+        $param = [
+            "index" => "elasticsearch",
+            "body" => [
+                "query" => [
+                    "regexp" => [
+                        "Content" => [
+                            "value" => ".*$content.*",
+                            "flags" => "ALL",
+                            "case_insensitive" => true
+                        ]
+                    ]
+                ]
+            ]
+        ];
+        $response = $client->search($param);
+
+
+        foreach($response['hits']['hits'] as $host) {
+            array_push($arr, $host['_id']);
+        }
+        return $this->render('search.html.twig', ['search_results' => $arr, 'content'=>$content]);
+
+
     }
     /**
-     * @Route("/google", name="app_homepage")
+     * @Route ("/view", name="dada")
+     * @param Request $request
      */
 
-    public function google(){
-        return $this->render('test.html.twig');
+    public function displayFile(Request $request): Response{
+
+
+        $name = $request->query->get('file');
+        $pdf = file_get_contents("../var/uploads/$name.pdf");
+        return new Response($pdf, 200, [
+            'Content-Type' => 'application/pdf',
+            'Content-Disposition' => "inline; filename = '$name.pdf'"
+        ]);
     }
+
+
 }
