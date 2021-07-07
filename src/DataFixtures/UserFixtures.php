db1e51733c7d10b9b4b1feb7bef3fac1d484c347
dzenzerovic <dzenzerovic@riteh.hr>
2021-07-07 14:21:39 +0200

    Added two more user fixtures for testing

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -31,8 +31,23 @@ class UserFixtures extends Fixture
         $user2->setMail("jsmith@example.com");
         $user2->setPassword($this->passwordEncoder->encodePassword($user2, 'abc123'));
 
+        $user3 = new User();
+        $user3->setFirstName("Pero");
+        $user3->setLastName("Perić");
+        $user3->setMail("pperic@example.com");
+        $user3->setPassword($this->passwordEncoder->encodePassword($user3, 'abc123'));
+        $user3->setRole('ROLE_ADMIN');
+
+        $user4 = new User();
+        $user4->setFirstName("John");
+        $user4->setLastName("Doe");
+        $user4->setMail("jdoe@example.com");
+        $user4->setPassword($this->passwordEncoder->encodePassword($user4, 'pass123'));
+
         $manager->persist($user);
         $manager->persist($user2);
+        $manager->persist($user3);
+        $manager->persist($user4);
 
         $manager->flush();
     }
